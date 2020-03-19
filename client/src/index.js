251c889a435b6ef9484054ab17bdc16bfe3efabd
Radek <radek@MacBook-Pro-Radek.local>
2020-03-19 21:02:08 +0100

    styles

--- client/src/index.js
+++ client/src/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import "./sass/styles.scss";
+import "./sass/main.scss";
 import App from "./App";
 import registerServiceWorker from "./registerServiceWorker";
 
