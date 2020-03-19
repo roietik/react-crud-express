ef0e87cad4b8f27db29d3f43bac7839d66ec2e80
Radek <radek@MacBook-Pro-Radek.local>
2020-03-19 18:40:51 +0100

    init

---
+++ client/src/App.js
@@ -0,0 +1,20 @@
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import "./App.scss";
+import Customers from "./components/customers";
+
+class App extends Component {
+  render() {
+    return (
+      <div className="app">
+        <header className="app__header">
+          <img src={logo} className="app__header--logo" alt="logo" />
+          <h1 className="app__header--title">React Express Starter</h1>
+        </header>
+        <Customers />
+      </div>
+    );
+  }
+}
+
+export default App;
